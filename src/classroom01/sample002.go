c524555a78842a3dc05ed0eeb7798ba392e62a15
Ronald <ronald@ntopus.com>
2020-02-25 20:12:42 +0000

    print text with colors in bash

---
+++ src/classroom01/sample002.go
@@ -0,0 +1,9 @@
+package main
+
+/**
+  ANSI escape is what can help us make visual changes
+  https://en.wikipedia.org/wiki/ANSI_escape_code
+*/
+func main() {
+	println("\033[1;31mOi\033[0m, Consigo escrever \033[1;33mColorido\033[0m no terminal!")
+}
