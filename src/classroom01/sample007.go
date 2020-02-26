ce9f43dd199622fcfb67cd0d56ea9b01d1b25e26
Ronald <ronald@ntopus.com>
2020-02-26 01:48:58 +0000

    go rotine to random print

---
+++ src/classroom01/sample007.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"sync"
+	"strconv"
+  "os"
+)
+
+/**
+  Unicode / UTF8 Characters
+  https://en.wikipedia.org/wiki/UTF-8
+  https://en.wikipedia.org/wiki/Unicode#UTF
+  https://en.wikibooks.org/wiki/Unicode/Character_reference/2000-2FFF
+*/
+const pentagon = "\u2B1F"
+const hexagon = "\u2B22"
+const circle = "\u2B24"
+const elipse = "\u2B2E"
+const black = "30"
+const red = "31"
+const green = "32"
+const yellow = "33"
+const blue = "34"
+const purple = "35"
+const lightblue = "36"
+const gray = "37"
+
+var wg = sync.WaitGroup{}
+
+func main() {
+  colorList := []string {
+    yellow,
+		blue,
+    purple,
+    gray,
+  }
+  textList := []string {
+    pentagon,
+    elipse,
+    hexagon,
+    circle,
+  }
+  defer println("")
+  defer wg.Wait()
+  argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg)>0 {
+		loopTimes, err := strconv.Atoi(argsWithoutProg[0])
+		if err != nil {
+      println("Fail to undestand Loop Times Value:")
+      panic(err)
+    }
+    i:= 0;
+    for  i < loopTimes {
+      printAllColoredTextInList(textList, colorList); i++
+    } 
+    return
+	}
+  for {
+    printAllColoredTextInList(textList, colorList)
+  }
+}
+
+func printAllColoredTextInList(textList, colorList [] string) {
+  for index := 0; index < len(textList); index++ {
+    wg.Add(1)
+    go func(i int) {
+      printExpecificColoredTextForEachColorInList(textList[i], colorList)
+      wg.Done()
+    }(index)
+  }
+}
+
+func printExpecificColoredTextForEachColorInList(text string, colorList []string) {
+	for _, color := range colorList {
+    if text == pentagon {
+      if color != yellow { continue }  
+      printColoredText(text+" ", color)
+    } else if color == purple {
+      if text == elipse { continue }  
+      wg.Add(1)
+      func(c string) { printColoredText(text+" ", c); wg.Done() }(color)
+    } else {
+      wg.Add(1)
+      func(c string) { 
+        printColoredText(text+" ", c)
+        wg.Done() 
+      }(color)
+      if text == circle { return }
+    }
+	}
+}
+
+func printColoredText(text string, colorCode string) {
+	print(mountColoredText(text, colorCode))
+}
+
+/**
+  ANSI escape is what can help us make visual changes
+  https://en.wikipedia.org/wiki/ANSI_escape_code
+*/
+func mountColoredText(text, colorCode string) string {
+	CSIColorClear := "\033[0m"
+	CSIColorStart := "\033[1;" + colorCode + "m"
+	return CSIColorStart + text + CSIColorClear
+}
